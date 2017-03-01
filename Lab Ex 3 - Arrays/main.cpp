1e7d3138330d1d31b99dcb281902e5bb31deaca4
Gams <500146979@fvtc.edu>
2017-02-28 19:10:55 -0600

    Finished

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -1,14 +1,15 @@
+// Michael Gams, Abhi Kumar, Michael C, Chris V
 
 /*
-[ ] Write a program that performs the following:
+[x] Write a program that performs the following:
 [x] Prompt the user to input five numbers.
 [x] Store the input into either an array or a vector.
 [x] Once the user is finished entering numbers:
-[] Display the five numbers in reverse order.
+[x] Display the five numbers in reverse order.
 [] Display the highest number entered.
 [] Display the lowest number entered.
 [x] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
-[ ] Compress(zip) your program and submit it to Blackboard.
+[x] Compress(zip) your program and submit it to Blackboard.
 */
 
 #include <iostream>
@@ -32,11 +33,34 @@ int main()
 
 	int i = (size - 1);
 
-	while (i > 0)
+	cout << "Reversed: ";
+	while (i >= 0)
 	{
-		cout << arr[i];
+		cout << arr[i] << " ";
 		i--;
 	}
+	cout << endl;
+	
+	i = 0;
+	int min = arr[i];
+	int max = arr[i];
+
+	while (i < size - 1)
+	{
+		i++;
+
+		if (arr[i] >= max)
+		{
+			max = arr[i];
+		}
+
+		if (arr[i] <= min)
+		{
+			min = arr[i];
+		}
+	}
+	cout << "Lowest Number: " << min << endl;
+	cout << "Highest Number: " << max << endl;
 
 	_getch();
 	return 0;
