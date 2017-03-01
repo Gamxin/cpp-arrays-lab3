9d2fe4035e1ee2cb8cebbea71fa520405ab7347d
Gams <500146979@fvtc.edu>
2017-02-28 18:51:25 -0600

    done

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -1,16 +1,14 @@
 
-// Lab Exercise 3 - Arrays
-// Instructions:
 /*
-	[ ] Write a program that performs the following:
-		[ ] Prompt the user to input five numbers.
-		[ ] Store the input into either an array or a vector.
-		[x] Once the user is finished entering numbers:
-			[x] Display the five numbers in reverse order.
-			[x] Display the highest number entered.
-			[x] Display the lowest number entered.
-		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
-	[ ] Compress(zip) your program and submit it to Blackboard.
+[ ] Write a program that performs the following:
+[x] Prompt the user to input five numbers.
+[x] Store the input into either an array or a vector.
+[x] Once the user is finished entering numbers:
+[] Display the five numbers in reverse order.
+[] Display the highest number entered.
+[] Display the lowest number entered.
+[x] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
+[ ] Compress(zip) your program and submit it to Blackboard.
 */
 
 #include <iostream>
@@ -20,14 +18,25 @@ using namespace std;
 
 int main()
 {
-	const int SIZE = 5;
-	int arr[SIZE];
+	int size;
 
-	for (int i = 0; i < SIZE; i++)
+	cout << "How Many Numbers? ";
+	cin >> size;
+
+	int *arr = new int[size];
+	for (int i = 0; i < size; i++)
 	{
-
+		cout << "Enter Some Number: ";
+		cin >> arr[i];
 	}
 
+	int i = (size - 1);
+
+	while (i > 0)
+	{
+		cout << arr[i];
+		i--;
+	}
 
 	_getch();
 	return 0;
